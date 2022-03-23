517adf2d90ea78e550d87be7a3fe87d5d88b9945
s2466627 <christiansarungallo@student.utwente.nl>
2022-03-23 18:16:39 +0100

    correcting the script for not being able to parse the date

--- ReadandWriteSerials.py
+++ ReadandWriteSerials.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-
 import serial
 import gzip
 import re
 
 class File:
-    def __init__(self):
+    def _init_(self):
         self.current_date = None
         self.old_date = None
         self.buffer = []
         self.fid = None
 
     def ropen_file(self):
-        '''This function rotates a file (if file descriptor is open) or only open a new 
+        ''' This function rotates a file (if file descriptor is open) or only open a new 
         file descriptor is none is open '''
         if self.fid:
             self.fid.close()
@@ -23,34 +22,37 @@ class File:
 
     def write(self, message):
         ''' Adding received GPS message to the buffer '''
+        #print(message, self.current_date)
         self.buffer.append(message)
 
         ''' Looking for a new timestamp in GNRMC '''
-        if re.match("\$GNRMC.\d{6}\,.", message):
+        if re.match("\$GNRMC.*\d{6}\,.", message):
             try:
                 current_date = message.split(",")[9].strip()
+                #print(current_date)
 
                 if re.match("\d{6}", current_date):
                     self.current_date = current_date
                     
-            except:
-                pass
+            except Exception as E
+                raise (E)
 
         ''' Rotating and writing to a file if we know current date '''
         if self.current_date:
-            ''' Open a new file descriptor or rotate if date has changed '''
-            if not self.fid() or not self.current_date == self.old_date:
+            if not self.fid or not self.old_date or not self.current_date == self.old_date:
                 self.ropen_file()
 
-            ''' Popping buffer content to a file '''
-            while self.buffer():
-                self.fid.write(self.buffer.pop(0))
+            ''' Popping buffer content to a file if fid is open '''
+            if self.fid:
+                while self.buffer:
+                    self.fid.write(self.buffer.pop(0) + "\n")
 
     def close(self):
         self.fid.close()
 
 file=File()
 ser = serial.Serial('/dev/ttyAMA0', baudrate=9600)
+
 while True:
     try:
         file.write(ser.readline().strip())
