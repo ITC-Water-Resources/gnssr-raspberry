ebf7df73482b7d7199b8e574e6cbc3709ad32274
s2466627 <christiansarungallo@student.utwente.nl>
2022-03-23 10:02:19 +0100

    adapting the script to name the file based on the date from nmea message

--- ReadandWriteSerials.py
+++ ReadandWriteSerials.py
@@ -1,33 +1,60 @@
+#!/usr/bin/env python
+
 import serial
 import gzip
-import time
+import re
 
 class File:
     def __init__(self):
-        current_date = time.strftime(\"%Y-%m-%d\")
-        self.old_date = current_date
-        self.filenamegz = \"{}.gz\".format(current_date)
-        self.fid = gzip.open(self.filenamegz,'wt')
+        self.current_date = None
+        self.old_date = None
+        self.buffer = []
+        self.fid = None
+
+    def ropen_file(self):
+        '''This function rotates a file (if file descriptor is open) or only open a new 
+        file descriptor is none is open '''
+        if self.fid:
+            self.fid.close()
+
+        self.filenamegz = "{}-gnssr2.gz".format(self.current_date)
+        self.fid = gzip.open(self.filenamegz, 'wt')
+        self.old_date = self.current_date
 
     def write(self, message):
-        current_date = time.strftime(\"%Y-%m-%d\")
+        ''' Adding received GPS message to the buffer '''
+        self.buffer.append(message)
 
-        if not current_date == self.old_date:
-            self.old_date = current_date
-            self.fid.close()
-            self.filenamegz = \"{}.gz\".format(current_date)
-            self.fid = gzip.open(self.filenamegz,'wt')
+        ''' Looking for a new timestamp in GNRMC '''
+        if re.match("\$GNRMC.\d{6}\,.", message):
+            try:
+                current_date = message.split(",")[9].strip()
 
-        self.fid.write(message)
+                if re.match("\d{6}", current_date):
+                    self.current_date = current_date
+                    
+            except:
+                pass
+
+        ''' Rotating and writing to a file if we know current date '''
+        if self.current_date:
+            ''' Open a new file descriptor or rotate if date has changed '''
+            if not self.fid() or not self.current_date == self.old_date:
+                self.ropen_file()
+
+            ''' Popping buffer content to a file '''
+            while self.buffer():
+                self.fid.write(self.buffer.pop(0))
 
     def close(self):
         self.fid.close()
 
-file = File()
-ser = serial.Serial ('/dev/ttyAMA0', baudrate=9600)
+file=File()
+ser = serial.Serial('/dev/ttyAMA0', baudrate=9600)
 while True:
     try:
-        file.write(ser.readline())
+        file.write(ser.readline().strip())
+
     except KeyboardInterrupt:
         file.close()
         break
